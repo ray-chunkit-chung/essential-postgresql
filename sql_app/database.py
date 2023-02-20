9019bea9fee01f72d860ff1123d362a3bc67dbf6
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2023-02-20 12:37:18 +0900

    tidy up

--- sql_app/database.py
+++ sql_app/database.py
@@ -10,9 +10,6 @@ SQLALCHEMY_DATABASE_URI = PostgresDsn.build(
     host="postgres",
     path=f"/{'docker' or ''}",
 )
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://docker:docker@192.168.112.3/docker"
-# SQLALCHEMY_DATABASE_URL = "postgresql://docker:docker@localhost/docker"
 
 engine = create_engine(
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -21,6 +18,3 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-# uvicorn sql_app.main:app --reload
